405a629724860777cf2cc0e097cbad65c0992411
youngran.kwon <youngran.kwon@gsitm.com>
2022-08-08 08:34:15 +0900

    Initial commit

---
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,21 @@
+package hello.advanced.app.v0;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderRepositoryV0 {
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외발생");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
