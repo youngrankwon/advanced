6b28c169a25f2f9396a43e2f83d5f576b2210776
youngran.kwon <youngran.kwon@gsitm.com>
2022-08-08 14:55:40 +0900

    1. 예제만들기-p14

---
+++ src/main/java/hello/advanced/trace/TraceStatus.java
@@ -0,0 +1,27 @@
+package hello.advanced.trace;
+
+
+public class TraceStatus {
+
+    private final TraceId traceId;
+    private final Long startTimeMs;
+    private final String message;
+
+    public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
+        this.traceId = traceId;
+        this.startTimeMs = startTimeMs;
+        this.message = message;
+    }
+
+    public TraceId getTraceId() {
+        return traceId;
+    }
+
+    public Long getStartTimeMs() {
+        return startTimeMs;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
